6da8b377aba766b002f1bd0d42612ae14118881e
hi1280 <hi12801280@gmail.com>
2021-06-08 23:05:56 +0900

    Add string utils

--- example/string_utils.py
+++ example/string_utils.py
@@ -1,2 +1,9 @@
-def echo():
-  return "abc"
+def echo(a):
+  return a
+def to_snake_case(str):
+  result = ""
+  for i, s in enumerate(str):
+    if s.isupper() and i >= 1:
+      result += "_"
+    result += s.lower()
+  return result
--- test/test_string_utils.py
+++ test/test_string_utils.py
@@ -2,4 +2,10 @@ from example import string_utils
 
 class TestStringUtils:
   def test_echo(self):
-    assert string_utils.echo() == "abc"
+    assert string_utils.echo("abc") == "abc"
+  def test_snake_case_aaa_is_aaa(self):
+    assert string_utils.to_snake_case("abc") == "abc"
+  def test_snake_case_HelloWorld_is_hello_world(self):
+    assert string_utils.to_snake_case("HelloWorld") == "hello_world"
+  def test_snake_case_practiceJunit_is_practice_junit(self):
+    assert string_utils.to_snake_case("practiceJunit") == "practice_junit"
